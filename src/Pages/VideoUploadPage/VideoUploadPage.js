a184454975ff29bc7bb73ce4752e81f24d9ff02c
brynfrayne <91647976+brynfrayne@users.noreply.github.com>
2021-12-15 17:24:06 -0800

    created Home Page branch

---
+++ src/Pages/VideoUploadPage/VideoUploadPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class VideoUploadPage extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
