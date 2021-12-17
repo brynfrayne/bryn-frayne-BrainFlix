87552a50bd65a62006ec5be811655e52aa058a8f
brynfrayne <91647976+brynfrayne@users.noreply.github.com>
2021-12-16 21:28:22 -0800

    completed upload page

--- src/Pages/VideoUploadPage/VideoUploadPage.js
+++ src/Pages/VideoUploadPage/VideoUploadPage.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react'
-import Header from '../../Components/Header/Header'
+import uploadImage from "../../Assets/Images/Upload-video-preview.jpg";
+import Button from '../../Components/Button/Button';
+import './VideoUploadPage.scss';
+import uploadIcon from "../../Assets/Icons/upload.svg";
+import '../../Components/Button/Button.scss';
 
 export default class VideoUploadPage extends Component {
     render() {
         return (
-            <div>
-                <Header />
+            <div className="upload-page">
+                <h1>Upload Video</h1>
+                <p className="upload-page__subtitle">VIDEO THUMBNAIL</p>
+                <img className="upload-page__thumbnail" src={uploadImage}/>
+                
+                <form className="upload-page__form">
+                    <label className="upload-page__form-label">TITLE YOUR VIDEO</label>
+                    <input className="upload-page__form-input" placeholder="Add a title to your video"/>
+                    <label className="upload-page__form-label">ADD A VIDEO DESCRIPTION</label>
+                    <textarea className="upload-page__form-input upload-page__form-input--description" placeholder="Add a description to your video"  />
+                </form>
+                <Button  icon={uploadIcon} buttonText={"PUBLISH"}/>
+                <button className="button-cancel">CANCEL</button>
             </div>
         )
     }
