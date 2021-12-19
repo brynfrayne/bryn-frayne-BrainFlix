9860f143f89914a6cbd85d9707c4d5018eeb15bd
brynfrayne <91647976+brynfrayne@users.noreply.github.com>
2021-12-18 21:20:58 -0800

    finished upload-page

--- src/Pages/VideoUploadPage/VideoUploadPage.js
+++ src/Pages/VideoUploadPage/VideoUploadPage.js
@@ -10,18 +10,24 @@ export default class VideoUploadPage extends Component {
     render() {
         return (
             <div className="upload-page">
-                <h1>Upload Video</h1>
-                <p className="upload-page__subtitle">VIDEO THUMBNAIL</p>
-                <img className="upload-page__thumbnail" src={uploadImage} alt="upload thumbnail"/>
-                
-                <form className="upload-page__form">
-                    <label className="upload-page__form-label">TITLE YOUR VIDEO</label>
-                    <input className="upload-page__form-input" placeholder="Add a title to your video"/>
-                    <label className="upload-page__form-label">ADD A VIDEO DESCRIPTION</label>
-                    <textarea className="upload-page__form-input upload-page__form-input--description" placeholder="Add a description to your video"  />
-                </form>
-                <Button  icon={uploadIcon} buttonText={"PUBLISH"} buttonUrl={'/'}/>
-                <Link className="button-cancel" to='/'>CANCEL</Link>
+                <h1 className="upload-page__title">Upload Video</h1>
+                <div className="upload-page__image-form-wrapper">
+                    <div >
+                        <p className="upload-page__subtitle">VIDEO THUMBNAIL</p>
+                        <img className="upload-page__thumbnail" src={uploadImage} alt="upload thumbnail"/>
+                    </div>
+                    
+                    <form className="upload-page__form">
+                        <label className="upload-page__form-label">TITLE YOUR VIDEO</label>
+                        <input className="upload-page__form-input" placeholder="Add a title to your video"/>
+                        <label className="upload-page__form-label">ADD A VIDEO DESCRIPTION</label>
+                        <textarea className="upload-page__form-input upload-page__form-input--description" placeholder="Add a description to your video"  />
+                    </form>
+                </div>    
+                <div className="upload-page__button-wrapper">
+                    <Button  icon={uploadIcon} buttonText={"PUBLISH"} buttonUrl={'/'} />
+                    <Link className="button-cancel" to='/'>CANCEL</Link>
+                </div>    
             </div>
         )
     }
