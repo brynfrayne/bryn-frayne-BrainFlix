0b64e69f9ffc977f29f9e13f168871349c0ae314
brynfrayne <91647976+brynfrayne@users.noreply.github.com>
2021-12-18 21:29:07 -0800

    final commit

--- src/util.js
+++
@@ -1,8 +0,0 @@
-export const formattedDate = (timestampDate) => {
-    const dateMS = timestampDate;
-    const date = new Date(dateMS);
-    const newDate = (date.getMonth() + 1) + "/" + (date.getDate()) + "/" +  date.getFullYear();
-    return newDate;
-}     
-
-// const filteredVideoList = this.state.videoDetails.filter((video) => video.id !== this.state.videos.id)
